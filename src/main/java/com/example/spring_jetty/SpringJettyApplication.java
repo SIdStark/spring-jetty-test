042ce5f89b4c0dde495406318efa6bbaa15e855d
Sid <siddharth.r.ravichandran@apisero.com>
2025-07-10 19:24:15 +0530

    New Project

---
+++ src/main/java/com/example/spring_jetty/SpringJettyApplication.java
@@ -0,0 +1,21 @@
+package com.example.spring_jetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJettyApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "Hello ";
+	}
+
+}
